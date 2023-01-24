26e0ebb8c9a84b30430acf84af55da803cd74a94
AngeloAG <angelo.ag.079@gmail.com>
2023-01-24 14:31:42 -0700

    Added option to exit program

--- prove/Develop02/SpamMenu.cs
+++ prove/Develop02/SpamMenu.cs
@@ -13,27 +13,32 @@ different actions in the journal.
 public class SpamMenu
 {
   /*TODO implement attributes to store the menu options*/
-  public List <String> _options = new List<String>();
+  public List<String> _options = new List<String>();
 
 
   public SpamMenu()
-  {_options.Add("1.Write") ;
-  _options.Add("2.Display");
-  _options.Add("3.Load");
-  _options.Add("4.Save");
+  {
+    _options.Add("1.Write");
+    _options.Add("2.Display");
+    _options.Add("3.Load");
+    _options.Add("4.Save");
+    _options.Add("5.Exit");
 
   }
 
   public void SpamDisplayMenu()
-  {for(int i=0;i<_options.Count;i++){
-    Console.WriteLine(_options[i]);
-  }
+  {
+    for (int i = 0; i < _options.Count; i++)
+    {
+      Console.WriteLine(_options[i]);
+    }
     /*To do implement method to display menu to the user*/
   }
 
   public String SpamAskForMenuSelection()
-  {String user_response = Console.ReadLine();
-  return user_response;
+  {
+    String user_response = Console.ReadLine();
+    return user_response;
 
     /*To do implement method to ask for menu selection from the terminal and return it*/
   }
