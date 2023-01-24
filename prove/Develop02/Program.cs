1be1b7773b099d73ff7c569c0e4fe6e3f7d54e56
AngeloAG <angelo.ag.079@gmail.com>
2023-01-24 14:31:56 -0700

    Added implementation of the main program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -27,9 +27,70 @@ class Program
   /*TODO implement attributes to store the jorunal instance, promptGenerator instance, and menu instance*/
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello Develop02 World!");
+    Console.WriteLine("Welcome to the Journal Program");
+    bool spamContinueAsking = true;
+
+    SpamPromptGenerator spamPromptGenerator = new SpamPromptGenerator();
+    SpamMenu spamMenu = new SpamMenu();
+    SpamJournal spamJournal = new SpamJournal();
+
+    do
+    {
+      Console.WriteLine("\nWhat do you want to do?");
+      spamMenu.SpamDisplayMenu();
+      string spamUserSelection = spamMenu.SpamAskForMenuSelection();
+
+      switch (spamUserSelection)
+      {
+        case "1":
+          //Option to write a new entry
+          string prompt = spamPromptGenerator.SpamGetRandomPrompt();
+          Console.WriteLine(prompt);
+          spamJournal.SpamWriteNewEntry(prompt);
+          break;
+        case "2":
+          //Option to Display the entries
+          spamJournal.SpamDisplay();
+          break;
+        case "3":
+          //Option to Load entries from file
+          try
+          {
+            Console.Write("Please enter the new name for the file: ");
+            string spamFileName = Console.ReadLine();
+            spamJournal.SpamLoadFromFile(spamFileName);
+          }
+          catch (Exception e)
+          {
+            Console.WriteLine($"Error {e}");
+          }
+          break;
+        case "4":
+          //Option to Save entries from file
+          try
+          {
+            Console.Write("Please enter the new name for the file: ");
+            string spamSaveFileName = Console.ReadLine();
+            spamJournal.SpamSaveToFile(spamSaveFileName);
+          }
+          catch (Exception e)
+          {
+            Console.WriteLine($"Error {e}");
+          }
+          break;
+        case "5":
+          //Option to close the program
+          spamContinueAsking = false;
+          break;
+        default:
+          Console.WriteLine("Please select an option from the menu. Just the number.");
+          continue;
+      }
+
+    } while (spamContinueAsking);
+
     SpamCloseProgram();
-    
+
   }
 
   static void SpamCloseProgram()
