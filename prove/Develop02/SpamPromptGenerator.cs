6b00a2a7bf52620a2db34fb455dc0cd36891b915
Jeffrey Meldrum <agentdex2@gmail.com>
2023-01-24 13:48:07 -0700

    changed stuff

--- prove/Develop02/SpamPromptGenerator.cs
+++ prove/Develop02/SpamPromptGenerator.cs
@@ -8,18 +8,30 @@ hold random prompts.
 Behaviors: Choose a random 
 prompt and give it back.
 */
+using System;
 
 public class SpamPromptGenerator
 {
   /*TODO create attributes to store the list of prompts*/
 
-  SpamPromptGenerator()
+  public SpamPromptGenerator()
   {
-
+    
   }
 
   public String SpamGetRandomPrompt()
   {
     /*TODO Implement the method to pick a prompt from the list and return it*/
+    List <string> SpamPromptList = new List<string>();
+    SpamPromptList.Add("What was the best part of your day?");
+    SpamPromptList.Add("What is something new you learned today?");
+    SpamPromptList.Add("What is something fun you did today?");
+    SpamPromptList.Add("How was the weather today?");
+    SpamPromptList.Add("What was your favorite meal today?");
+    
+    Random SpamRandomPromptNumber = new Random();
+    int SpamListIndex = SpamRandomPromptNumber.Next(0,4);
+    string SpamRandomPrompt = SpamPromptList[SpamListIndex];
+    return SpamRandomPrompt;
   }
 }
